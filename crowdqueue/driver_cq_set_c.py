0f4d16e37dde31655f854639a4a41db4fe396aba
Mohcine Chraibi <m.chraibi@fz-juelich.de>
2026-09-06 15:58:23 +0200

    fix: run Dakota motivation profiles on macOS

--- crowdqueue/driver_cq_set_c.py
+++ crowdqueue/driver_cq_set_c.py
@@ -5,8 +5,9 @@ import os
 import driver_cq
 
 
-os.environ["CQ_FIXED_PARAMS"] = (
-    '{"radius": 0.144, "strength_neighbor": 9.41, "range_neighbor": 0.09, '
-    '"strength_geometry": 2.60, "range_geometry": 0.032}'
-)
-driver_cq.main()
+if __name__ == "__main__":
+    os.environ["CQ_FIXED_PARAMS"] = (
+        '{"radius": 0.144, "strength_neighbor": 9.41, "range_neighbor": 0.09, '
+        '"strength_geometry": 2.60, "range_geometry": 0.032}'
+    )
+    driver_cq.main()
